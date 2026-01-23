f404b8fb29a029f61d7abf95271555c10601c9f8
anna <annachu211@gmail.com>
2026-01-22 23:38:52 -0600

    Update Stepper_Motors_Test.ino

--- Tests/Stepper_Motors_Test.ino
+++ Tests/Stepper_Motors_Test.ino
@@ -1,1 +1,16 @@
 // This program was used to test the stepper motors
+#include <Stepper.h>                   // the stepper library
+
+#define STEPS 2038                     // defining # of steps per revolution for motor
+Stepper stepper(STEPS, 8, 9, 10, 11);  // initialize stepper object and set pin connections (IN1, IN3, IN2, IN4)
+
+void setup() 
+{
+}
+
+void loop() 
+{
+  stepper.setSpeed(5);  // Rotate clockwise at 5 RPM
+  stepper.step(STEPS);  // Rotate one full revolution clockwise
+  delay(1000);          // Wait for 1 second
+}
