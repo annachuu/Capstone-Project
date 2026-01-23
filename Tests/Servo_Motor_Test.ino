c41cc247612c723626583c910a74fca2a5bfaa59
anna <annachu211@gmail.com>
2026-01-22 23:40:06 -0600

    Create Servo_Motor_Test.ino

---
+++ Tests/Servo_Motor_Test.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() 
+{
+  myServo.attach(10);  // Attach servo to pin 10
+}
+
+void loop() 
+{
+  myServo.write(0);    // Move to 0 degrees
+  delay(1000);
+  myServo.write(90);   // Move to 90 degrees
+  delay(1000);
+  myServo.write(180);  // Move to 180 degrees
+  delay(1000);
+}
